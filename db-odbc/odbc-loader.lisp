2dd501b064b97fed407e4ff9025e25e386394977
diego <dieggsy@pm.me>
2025-03-07 17:09:35 -0500

    Use cffi:foreign-library-loaded-p for ODBC instead of internal CLSQL variable

--- db-odbc/odbc-loader.lisp
+++ db-odbc/odbc-loader.lisp
@@ -21,23 +21,14 @@
   (:unix (:or "libodbc.so.2" "libodbc.so" "libiodbc.so.2" "libiodbc.so"))
   (:windows (:or "odbc32.dll" "odbc32.lib" "odbc32.so")))
 
-(defvar *odbc-supporting-libraries* '("c")
-  "Used only by CMU. List of library flags needed to be passed to ld to
-load the Odbc client library succesfully.  If this differs at your site,
-set to the right path before compiling or loading the system.")
-
-(defvar *odbc-library-loaded* nil
-  "T if foreign library was able to be loaded successfully")
-
 (defmethod clsql-sys:database-type-library-loaded ((database-type (eql :odbc)))
-  *odbc-library-loaded*)
+  (cffi:foreign-library-loaded-p 'libodbc))
 
 (defmethod clsql-sys:database-type-load-foreign ((database-type (eql :odbc)))
   (mapc (lambda (path)
           (pushnew path cffi:*foreign-library-directories* :test #'equal))
         clsql:*foreign-library-search-paths*)
-  (cffi:load-foreign-library 'libodbc)
-  (setq *odbc-library-loaded* t))
+  (cffi:load-foreign-library 'libodbc))
 
 (clsql-sys:database-type-load-foreign :odbc)
 
