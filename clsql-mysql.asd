sharplispers/clsql

commit a1136aa7f61ca7f004145cf67dc8755ec1ee15f7

Clean up asd files and modules
- Shouldn't need to define top-level defsystem packages
- Use actual directories for modules (I think this is less confusing)

clsql-mysql.asd
@@ -14,9 +14,6 @@
 ;;;; (http://opensource.franz.com/preamble.html), also known as the LLGPL.
 ;;;; *************************************************************************
 
-(defpackage #:clsql-mysql-system (:use #:asdf #:cl))
-(in-package #:clsql-mysql-system)
-
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (unless (find-package 'uffi)
     (asdf:operate 'asdf:load-op 'uffi)))

clsql-aodbc.asd
@@ -14,9 +14,6 @@
 ;;;; (http://opensource.franz.com/preamble.html), also known as the LLGPL.
 ;;;; *************************************************************************
 
-(defpackage #:clsql-aodbc-system (:use #:asdf #:cl))
-(in-package #:clsql-aodbc-system)
-
 #+(and allegro (not allegro-cl-trial))
 (defsystem clsql-aodbc
   :name "cl-sql-aodbc"

clsql-cffi.asd
@@ -14,9 +14,6 @@
 
 (push :clsql-cffi cl:*features*)
 
-(defpackage #:clsql-cffi-system (:use #:asdf #:cl))
-(in-package #:clsql-cffi-system)
-
 (defsystem clsql-cffi
     :name "CLSQL-CFFI"
     :author "Kevin Rosenberg <kevin@rosenberg.net>"

clsql-db2.asd
@@ -14,9 +14,6 @@
 ;;;; (http://opensource.franz.com/preamble.html), also known as the LLGPL.
 ;;;; *************************************************************************
 
-(defpackage #:clsql-db2-system (:use #:asdf #:cl))
-(in-package #:clsql-db2-system)
-
 ;;; System definition
 
 (defsystem clsql-db2

clsql-odbc.asd
@@ -14,9 +14,6 @@
 ;;;; (http://opensource.franz.com/preamble.html), also known as the LLGPL.
 ;;;; *************************************************************************
 
-(defpackage #:clsql-odbc-system (:use #:asdf #:cl))
-(in-package #:clsql-odbc-system)
-
 (defsystem clsql-odbc
   :name "clsql-odbc"
   :author "Kevin M. Rosenberg <kmr@debian.org>"

clsql-oracle.asd
@@ -14,9 +14,6 @@
 ;;;; (http://opensource.franz.com/preamble.html), also known as the LLGPL.
 ;;;; *************************************************************************
 
-(defpackage #:clsql-oracle-system (:use #:asdf #:cl))
-(in-package #:clsql-oracle-system)
-
 ;;; System definition
 
 (defsystem clsql-oracle

clsql-postgresql-socket.asd
@@ -14,9 +14,6 @@
 ;;;; (http://opensource.franz.com/preamble.html), also known as the LLGPL.
 ;;;; *************************************************************************
 
-(defpackage #:clsql-postgresql-socket-system (:use #:asdf #:cl))
-(in-package #:clsql-postgresql-socket-system)
-
 ;;; System definition
 
 (defsystem clsql-postgresql-socket

clsql-postgresql-socket3.asd
@@ -16,9 +16,6 @@
 ;;;; (http://opensource.franz.com/preamble.html), also known as the LLGPL.
 ;;;; *************************************************************************
 
-(defpackage #:clsql-postgresql-socket-system (:use #:asdf #:cl))
-(in-package #:clsql-postgresql-socket-system)
-
 ;;; System definition
 
 (defsystem clsql-postgresql-socket3

commit ae4defe018fe006ca3b94868ac5265512fe5bf32

clsql-mysql: Fix broken system load
I broke this in a1136aa7f61ca7f004145cf67dc8755ec1ee15f7

clsql-mysql.asd
@@ -14,6 +14,9 @@
 ;;;; (http://opensource.franz.com/preamble.html), also known as the LLGPL.
 ;;;; *************************************************************************
 
+(defpackage #:clsql-mysql-system (:use #:asdf #:cl))
+(in-package #:clsql-mysql-system)
+
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (unless (find-package 'uffi)
     (asdf:operate 'asdf:load-op 'uffi)))